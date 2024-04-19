cd371b073dcbf118b7f635ff668b860fcd709ca8
“orodriguez” <omarjavier15@gmail.com>
2024-04-19 16:10:56 -0400

    Assignment 3

---
+++ Tests/TwoSumTests.cs
@@ -0,0 +1,31 @@
+namespace Tests;
+
+// Read: https://leetcode.com/problems/two-sum/description/
+public class TwoSumTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(new[] { 0, 1 },
+            TwoSum(new[] { 2, 6, 7, 11, 15 }, 9));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        Assert.Equal(new[] { 1, 4 },
+            TwoSum(new[] { 3, 2, 4 }, 6));
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        Assert.Equal(new[] { 0, 1 },
+            TwoSum(new[] { 3, 6 }, 6));
+    }
+
+    private int[] TwoSum(int[] nums, int target)
+    {
+        throw new NotImplementedException();
+    }
+}
