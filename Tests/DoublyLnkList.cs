1fec3f32f5c772173ebe2401ab16f536162ca603
“orodriguez” <omarjavier15@gmail.com>
2024-04-19 11:26:07 -0400

    DoublyLnkList.Last

--- Tests/DoublyLnkList.cs
+++ Tests/DoublyLnkList.cs
@@ -6,17 +6,53 @@ public class DoublyLnkList<T> : ILnkList<T>
 {
     public static DoublyLnkList<T> From(params T[] values)
     {
-        throw new NotImplementedException();
+        var list = new DoublyLnkList<T>();
+
+        foreach (var value in values)
+            list.Append(value);
+
+        return list;
+    }
+
+    private DoublyLnkNode? _head;
+    private DoublyLnkNode? _last;
+
+    public int Count { get; private set; }
+
+    public DoublyLnkList()
+    {
+        _head = null;
+        _last = null;
     }
 
     public void Prepend(T value)
     {
-        throw new NotImplementedException();
+        if (_head == null)
+        {
+            _head = new DoublyLnkNode(value);
+            _last = _head;
+            Count++;
+            return;
+        }
+
+        _head = new DoublyLnkNode(value, _head);
+
+        Count++;
     }
 
+    // O(1)
     public void Append(T value)
     {
-        throw new NotImplementedException();
+        if (_head == null)
+        {
+            _head = new DoublyLnkNode(value);
+            _last = _head;
+            Count++;
+            return;
+        }
+
+        _last = new DoublyLnkNode(value, next: null, previous: _last);
+        Count++;
     }
 
     public T First()
@@ -35,8 +71,6 @@ public class DoublyLnkList<T> : ILnkList<T>
     {
         throw new NotImplementedException();
     }
-
-    public int Count => throw new NotImplementedException();
 
     public IEnumerable<T> ToEnumerable()
     {
@@ -66,5 +100,44 @@ public class DoublyLnkList<T> : ILnkList<T>
     IEnumerator IEnumerable.GetEnumerator()
     {
         return GetEnumerator();
+    }
+
+    public T Last()
+    {
+        if (_last == null)
+            throw new InvalidOperationException();
+        
+        return _last.Value;
+    }
+
+    private class DoublyLnkNode
+    {
+        public T Value { get; }
+        public DoublyLnkNode? Next { get; set; }
+        public DoublyLnkNode? Previous { get; private set; }
+
+        public DoublyLnkNode(T value, 
+            DoublyLnkNode? next = null,
+            DoublyLnkNode? previous = null)
+        {
+            Value = value;
+            Next = next;
+            Previous = previous;
+            
+            if (next != null)
+                next.Previous = this;
+
+            if (previous != null)
+                previous.Next = this;
+        }
+        
+        public bool NextValueEquals(T value) =>
+            Next != null && Next.ValueEquals(value);
+
+        public bool ValueEquals(T value) =>
+            Value != null && Value.Equals(value);
+
+        public void AddAfter(T value) => 
+            Next = new DoublyLnkNode(value, next: null, previous: this);
     }
 }
--- Tests/DoublyLnkListTests.cs
+++ Tests/DoublyLnkListTests.cs
@@ -6,6 +6,36 @@ public class DoublyLnkListTests : AbstractLnkListTests
     protected override ILnkList<T> CreateLinkedList<T>(params T[] values) => DoublyLnkList<T>.From(values);
 
     [Fact]
+    public void Last_One()
+    {
+        var ll = new DoublyLnkList<int>();
+
+        ll.Append(1);
+
+        Assert.Equal(1, ll.Last());
+    }
+    
+    [Fact]
+    public void Last_Two()
+    {
+        var ll = new DoublyLnkList<int>();
+
+        ll.Append(1);
+        ll.Append(2);
+
+        Assert.Equal(2, ll.Last());
+    }
+    
+    [Fact]
+    public void Last_Empty()
+    {
+        var ll = new DoublyLnkList<int>();
+
+        Assert.Throws<InvalidOperationException>(
+            () => ll.Last());
+    }
+
+    [Fact]
     public void ToReversedEnumerable()
     {
         var ll = DoublyLnkList<int>.From(1, 2, 3);
