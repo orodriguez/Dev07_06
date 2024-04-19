85e5d4ff3e0f752b5fcca3c456a16753fea70c90
“orodriguez” <omarjavier15@gmail.com>
2024-04-19 12:55:16 -0400

    Implement DoublyLnklistTests

--- Tests/DoublyLnkList.cs
+++ Tests/DoublyLnkList.cs
@@ -55,58 +55,163 @@ public class DoublyLnkList<T> : ILnkList<T>
         Count++;
     }
 
-    public T First()
-    {
-        throw new NotImplementedException();
-    }
-
-    public bool Any(Func<T, bool> compare)
-    {
-        throw new NotImplementedException();
-    }
-
-    public T this[int index] => throw new NotImplementedException();
+    public T this[int index] => Get(index);
 
     public T Get(int index)
     {
-        throw new NotImplementedException();
+        // Ω(1)
+        if (_head == null)
+            throw new IndexOutOfRangeException();
+
+        // Ω(1)
+        if (index < 0)
+            throw new IndexOutOfRangeException();
+
+        // O(1)
+        if (index == Count - 1 && _last != null)
+            return _last.Value;
+
+        var currentIndex = 0;
+
+        foreach (var value in this)
+        {
+            if (currentIndex == index)
+                return value;
+            currentIndex++;
+        }
+
+        throw new IndexOutOfRangeException();
     }
 
-    public IEnumerable<T> ToEnumerable()
+    public IEnumerable<T> ToReversedEnumerable()
     {
-        throw new NotImplementedException();
-    }
+        var result = new List<T>();
+        var current = _last;
+        while (current != null)
+        {
+            result.Add(current.Value);
+            current = current.Previous;
+        }
 
-    public IEnumerable<int> ToReversedEnumerable()
-    {
-        throw new NotImplementedException();
+        return result;
     }
 
     public bool RemoveAt(int index)
     {
-        throw new NotImplementedException();
+        // Ω(1)
+        if (index < 0)
+            return false;
+
+        // Ω(1)
+        if (_head == null)
+            return false;
+
+        // Ω(1)
+        if (index == 0)
+        {
+            _head = _head.Next;
+
+            if (_head != null)
+                _head.Previous = null;
+
+            Count--;
+            return true;
+        }
+
+        if (index == Count - 1 && _last != null)
+        {
+            _last = _last.Previous;
+            
+            if (_last != null)
+                _last.Next = null;
+
+            return true;
+        }
+
+        var currentIndex = 0;
+        var current = _head;
+
+        // Ω(1)
+        // O(n - 1)
+        while (current != null)
+        {
+            if (currentIndex == index)
+            {
+                if (current.Next != null)
+                    current.Next.Previous = current.Previous;
+
+                if (current.Previous != null)
+                    current.Previous.Next = current.Next;
+
+                Count--;
+                return true;
+            }
+
+            currentIndex++;
+            current = current.Next;
+        }
+
+        return false;
     }
 
     public bool Remove(T value)
     {
-        throw new NotImplementedException();
+        if (_head == null)
+            return false;
+
+        if (_head.ValueEquals(value))
+        {
+            _head = _head.Next;
+
+            if (_head != null)
+                _head.Previous = null;
+
+            Count--;
+            return true;
+        }
+        
+        if (_last != null && _last.ValueEquals(value))
+        {
+            _last = _last.Previous;
+            
+            if (_last != null)
+                _last.Next = null;
+
+            return true;
+        }
+
+        var current = _head;
+        while (current != null)
+        {
+            if (current.ValueEquals(value))
+            {
+                if (current.Previous != null)
+                    current.Previous.Next = current.Next;
+
+                if (current.Next != null)
+                    current.Next.Previous = current.Previous;
+
+                Count--;
+                return true;
+            }
+
+            current = current.Next;
+        }
+
+        return false;
     }
 
-    public IEnumerator<T> GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
+    public IEnumerator<T> GetEnumerator() =>
+        new NodeEnumerator(_head);
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
+    IEnumerator IEnumerable.GetEnumerator() =>
+        GetEnumerator();
 
     public T Last()
     {
         if (_last == null)
             throw new InvalidOperationException();
-        
+
         return _last.Value;
     }
 
@@ -114,30 +219,61 @@ public class DoublyLnkList<T> : ILnkList<T>
     {
         public T Value { get; }
         public DoublyLnkNode? Next { get; set; }
-        public DoublyLnkNode? Previous { get; private set; }
+        public DoublyLnkNode? Previous { get; set; }
 
-        public DoublyLnkNode(T value, 
+        public DoublyLnkNode(T value,
             DoublyLnkNode? next = null,
             DoublyLnkNode? previous = null)
         {
             Value = value;
             Next = next;
             Previous = previous;
-            
+
             if (next != null)
                 next.Previous = this;
 
             if (previous != null)
                 previous.Next = this;
         }
-        
+
         public bool NextValueEquals(T value) =>
             Next != null && Next.ValueEquals(value);
 
         public bool ValueEquals(T value) =>
             Value != null && Value.Equals(value);
 
-        public void AddAfter(T value) => 
+        public void AddAfter(T value) =>
             Next = new DoublyLnkNode(value, next: null, previous: this);
+    }
+
+    private class NodeEnumerator : IEnumerator<T>
+    {
+        private readonly DoublyLnkNode _head;
+        private DoublyLnkNode _currentNode;
+
+        public NodeEnumerator(DoublyLnkNode node)
+        {
+            _head = node;
+            _currentNode = new DoublyLnkNode(default, node);
+        }
+
+        public T Current => _currentNode.Value;
+
+        public bool MoveNext()
+        {
+            if (_currentNode.Next == null)
+                return false;
+            _currentNode = _currentNode.Next;
+            return true;
+        }
+
+        public void Reset() =>
+            _currentNode = new DoublyLnkNode(default, _head);
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+        }
     }
 }
--- Tests/DoublyLnkListTests.cs
+++ Tests/DoublyLnkListTests.cs
@@ -14,7 +14,7 @@ public class DoublyLnkListTests : AbstractLnkListTests
 
         Assert.Equal(1, ll.Last());
     }
-    
+
     [Fact]
     public void Last_Two()
     {
@@ -25,7 +25,7 @@ public class DoublyLnkListTests : AbstractLnkListTests
 
         Assert.Equal(2, ll.Last());
     }
-    
+
     [Fact]
     public void Last_Empty()
     {
@@ -36,10 +36,61 @@ public class DoublyLnkListTests : AbstractLnkListTests
     }
 
     [Fact]
+    public void GetByIndex_Last()
+    {
+        var ll = new DoublyLnkList<int>();
+
+        ll.Append(1);
+        ll.Append(2);
+
+        Assert.Equal(2, ll[1]);
+    }
+
+    [Fact]
     public void ToReversedEnumerable()
     {
         var ll = DoublyLnkList<int>.From(1, 2, 3);
-        
-        Assert.Equal(new[] {3, 2, 1}, ll.ToReversedEnumerable());
+
+        Assert.Equal(new[] { 3, 2, 1 }, ll.ToReversedEnumerable());
+    }
+
+    [Fact]
+    public void ToReversedEnumerable_Append()
+    {
+        var ll = DoublyLnkList<int>.From(1, 2, 3);
+
+        ll.Append(4);
+
+        Assert.Equal(new[] { 4, 3, 2, 1 }, ll.ToReversedEnumerable());
+    }
+
+    [Fact]
+    public void ToReversedEnumerable_Prepend()
+    {
+        var ll = DoublyLnkList<int>.From(1, 2, 3);
+
+        ll.Prepend(0);
+
+        Assert.Equal(new[] { 3, 2, 1, 0 }, ll.ToReversedEnumerable());
+    }
+
+    [Fact]
+    public void ToReversedEnumerable_RemoveAt()
+    {
+        var ll = DoublyLnkList<int>.From(1, 2, 3);
+
+        ll.RemoveAt(2);
+
+        Assert.Equal(new[] { 2, 1 }, ll.ToReversedEnumerable());
+    }
+    
+    [Fact]
+    public void ToReversedEnumerable_Remove()
+    {
+        var ll = DoublyLnkList<int>.From(1, 2, 3);
+
+        ll.Remove(3);
+
+        Assert.Equal(new[] { 2, 1 }, ll.ToReversedEnumerable());
     }
 }
