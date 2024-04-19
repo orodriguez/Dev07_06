d50090c2d7d89c7bbe0a3b6a30d7833400288e6e
fernandito809 <127434636+fernandito809@users.noreply.github.com>
2024-04-19 06:46:52 -0400

    Add files via upload
    Had trouble declaring two properties pointing to the first and last element

---
+++ DoublyLnkList.cs
@@ -0,0 +1,66 @@
+using System.Text.RegularExpressions;
+
+namespace Tests;
+
+public class DoublyLnkList<T> : ILnkList<T>
+
+
+{
+    public static DoublyLnkList<T> From(params T[] values)
+    
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void Prepend(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Append(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T First()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Any(Func<T, bool> compare)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T this[int index] => throw new NotImplementedException();
+
+    public T Get(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Count()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> ToEnumerable()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<int> ToReversedEnumerable()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(T value)
+    {
+        throw new NotImplementedException();
+    }
+}
