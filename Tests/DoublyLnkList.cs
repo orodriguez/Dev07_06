3b7c517b3da223b9ade7ea29249fc5aeddf15c7f
cesar <you@example.com>
2024-04-19 08:00:43 -0400

    tarea_2

--- Tests/DoublyLnkList.cs
+++ Tests/DoublyLnkList.cs
@@ -1,61 +1,219 @@
-namespace Tests;
-
-public class DoublyLnkList<T> : ILnkList<T>
+namespace Tests
 {
-    public static DoublyLnkList<T> From(params T[] values)
+    public class DoublyLnkList<T> : ILnkList<T>
     {
-        throw new NotImplementedException();
-    }
+        private class Node
+        {
+            public T Value { get; set; }
+            public Node? Next { get; set; }
+            public Node? Previous { get; set; }
 
-    public void Prepend(T value)
-    {
-        throw new NotImplementedException();
-    }
+            public Node(T value)
+            {
+                Value = value;
+                Next = null;
+                Previous = null;
+            }
+        }
 
-    public void Append(T value)
-    {
-        throw new NotImplementedException();
-    }
+        private Node? _head;
+        private Node? _last;
+        private int _count;
 
-    public T First()
-    {
-        throw new NotImplementedException();
-    }
+        public DoublyLnkList()
+        {
+            _head = null;
+            _last = null;
+            _count = 0;
+        }
 
-    public bool Any(Func<T, bool> compare)
-    {
-        throw new NotImplementedException();
-    }
+        public static DoublyLnkList<T> From(params T[] values)
+        {
+            var newList = new DoublyLnkList<T>();
+            foreach (var value in values)
+            {
+                newList.Append(value);
+            }
+            return newList;
+        }
 
-    public T this[int index] => throw new NotImplementedException();
+        public void Prepend(T value)
+        {
+            var newNode = new Node(value);
+            if (_head == null)
+            {
+                _head = newNode;
+                _last = newNode;
+            }
+            else
+            {
+                newNode.Next = _head;
+                _head.Previous = newNode;
+                _head = newNode;
+            }
+            _count++;
+        }
 
-    public T Get(int index)
-    {
-        throw new NotImplementedException();
-    }
+        public void Append(T value)
+        {
+            var newNode = new Node(value);
+            if (_last == null)
+            {
+                _head = newNode;
+                _last = newNode;
+            }
+            else
+            {
+                newNode.Previous = _last;
+                _last.Next = newNode;
+                _last = newNode;
+            }
+            _count++;
+        }
 
-    public int Count()
-    {
-        throw new NotImplementedException();
-    }
+        public T First()
+        {
+            if (_head == null)
+                throw new InvalidOperationException();
+            return _head.Value;
+        }
 
-    public IEnumerable<T> ToEnumerable()
-    {
-        throw new NotImplementedException();
-    }
+        public bool Any(Func<T, bool> compare)
+        {
+            var current = _head;
+            while (current != null)
+            {
+                if (compare(current.Value))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
 
-    public IEnumerable<int> ToReversedEnumerable()
-    {
-        throw new NotImplementedException();
-    }
+        public T Get(int index)
+        {
+            if (index < 0 || index >= _count)
+            {
+                throw new IndexOutOfRangeException();
+            }
 
-    public bool RemoveAt(int index)
-    {
-        throw new NotImplementedException();
-    }
+            var current = _head;
+            for (int i = 0; i < index; i++)
+            {
+                if (current == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                current = current.Next;
+            }
 
-    public bool Remove(T value)
-    {
-        throw new NotImplementedException();
+            if (current == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return current.Value;
+        }
+
+        public int Count()
+        {
+            return _count;
+        }
+
+        public IEnumerable<T> ToEnumerable()
+        {
+            var current = _head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerable<T> ToReversedEnumerable()
+        {
+            var current = _last;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Previous;
+            }
+        }
+
+        public bool RemoveAt(int index)
+        {
+            if (index < 0 || index >= _count)
+                return false;
+
+            var current = _head;
+            for (int i = 0; i < index; i++)
+            {
+                if (current == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                current = current.Next;
+            }
+
+            if (current == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (current == _head)
+            {
+                _head = current.Next;
+                if (_head != null)
+                    _head.Previous = null;
+            }
+            else if (current == _last)
+            {
+                _last = current.Previous;
+                if (_last != null)
+                    _last.Next = null;
+            }
+            else
+            {
+                if (current.Previous != null)
+                    current.Previous.Next = current.Next;
+                if (current.Next != null)
+                    current.Next.Previous = current.Previous;
+            }
+
+            _count--;
+            return true;
+        }
+
+        public bool Remove(T value)
+        {
+            var current = _head;
+            while (current != null)
+            {
+                if (EqualityComparer<T>.Default.Equals(current.Value, value))
+                {
+                    if (current == _head)
+                        _head = current.Next;
+
+                    if (current == _last)
+                        _last = current.Previous;
+
+                    if (current.Previous != null)
+                        current.Previous.Next = current.Next;
+
+                    if (current.Next != null)
+                        current.Next.Previous = current.Previous;
+
+                    _count--;
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public T this[int index]
+        {
+            get { return Get(index); }
+        }
     }
 }
