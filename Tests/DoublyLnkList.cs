db559f120cd3d61632b850d47a3c2e5418561bbf
Abel Garcia <agtapia234@gmail.com>
2024-04-18 21:58:10 -0400

    DoubleLinkList

--- Tests/DoublyLnkList.cs
+++ Tests/DoublyLnkList.cs
@@ -2,60 +2,198 @@ namespace Tests;
 
 public class DoublyLnkList<T> : ILnkList<T>
 {
+    private class Node
+    {
+        public T Value { get; }
+        public Node? Previous { get; set; }
+        public Node? Next { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+    }
+
+    private Node? _head;
+    private Node? _tail;
+    private int _count;
+
     public static DoublyLnkList<T> From(params T[] values)
     {
-        throw new NotImplementedException();
+        var list = new DoublyLnkList<T>();
+        foreach (var value in values)
+        {
+            list.Append(value);
+        }
+        return list;
     }
 
     public void Prepend(T value)
     {
-        throw new NotImplementedException();
+        Node newNode = new Node(value);
+        if (_head == null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Next = _head;
+            _head.Previous = newNode;
+            _head = newNode;
+        }
+        _count++;
     }
 
     public void Append(T value)
     {
-        throw new NotImplementedException();
+        Node newNode = new Node(value);
+        if (_tail == null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            _tail.Next = newNode;
+            newNode.Previous = _tail;
+            _tail = newNode;
+        }
+        _count++;
     }
 
     public T First()
     {
-        throw new NotImplementedException();
+        if (_head == null)
+            throw new InvalidOperationException("The list is empty.");
+        return _head.Value;
     }
 
     public bool Any(Func<T, bool> compare)
     {
-        throw new NotImplementedException();
+        Node? current = _head;
+        while (current != null)
+        {
+            if (compare(current.Value))
+                return true;
+            current = current.Next;
+        }
+        return false;
     }
 
-    public T this[int index] => throw new NotImplementedException();
+    public T this[int index]
+    {
+        get
+        {
+            return Get(index);
+        }
+    }
 
     public T Get(int index)
     {
-        throw new NotImplementedException();
+        if (index < 0 || index >= _count)
+            throw new IndexOutOfRangeException();
+
+        Node? current = _head;
+        for (int i = 0; i < index; i++)
+        {
+            current = current!.Next;
+        }
+        return current!.Value;
     }
 
     public int Count()
     {
-        throw new NotImplementedException();
+        return _count;
     }
 
     public IEnumerable<T> ToEnumerable()
     {
-        throw new NotImplementedException();
+        Node? current = _head;
+        while (current != null)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
     }
 
-    public IEnumerable<int> ToReversedEnumerable()
+    public IEnumerable<T> ToReversedEnumerable()
     {
-        throw new NotImplementedException();
+        Node? current = _tail;
+        while (current != null)
+        {
+            yield return current.Value;
+            current = current.Previous;
+        }
     }
 
     public bool RemoveAt(int index)
     {
-        throw new NotImplementedException();
+        if (index < 0 || index >= _count)
+            return false;
+
+        Node? current = _head;
+        for (int i = 0; i < index; i++)
+        {
+            current = current!.Next;
+        }
+
+        if (current == _head)
+        {
+            _head = current!.Next;
+        }
+        else
+        {
+            current!.Previous!.Next = current.Next;
+        }
+
+        if (current == _tail)
+        {
+            _tail = current.Previous;
+        }
+        else
+        {
+            current.Next!.Previous = current.Previous;
+        }
+
+        _count--;
+        return true;
     }
 
     public bool Remove(T value)
     {
-        throw new NotImplementedException();
+        Node? current = _head;
+        while (current != null)
+        {
+            if (EqualityComparer<T>.Default.Equals(current.Value, value))
+            {
+                if (current == _head)
+                {
+                    _head = current.Next;
+                    if (_head != null)
+                        _head.Previous = null;
+                }
+                else
+                {
+                    current.Previous!.Next = current.Next;
+                }
+
+                if (current == _tail)
+                {
+                    _tail = current.Previous;
+                    if (_tail != null)
+                        _tail.Next = null;
+                }
+                else
+                {
+                    current.Next!.Previous = current.Previous;
+                }
+
+                _count--;
+                return true;
+            }
+            current = current.Next;
+        }
+        return false;
     }
 }
