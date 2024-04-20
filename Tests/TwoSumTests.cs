895cb2ac9876138e10c47e850d7a85e8c936c743
Manases Lovera <manaseslovera07@gmail.com>
2024-04-20 13:30:34 -0400

    all test pass

--- Tests/TwoSumTests.cs
+++ Tests/TwoSumTests.cs
@@ -6,26 +6,37 @@ public class TwoSumTests
     [Fact]
     public void Test1()
     {
-        Assert.Equal(new[] { 0, 1 },
+        Assert.Equal(new[] { 0, 2 },
             TwoSum(new[] { 2, 6, 7, 11, 15 }, 9));
     }
 
     [Fact]
     public void Test2()
     {
-        Assert.Equal(new[] { 1, 4 },
+        Assert.Equal(new[] { 1, 2 },
             TwoSum(new[] { 3, 2, 4 }, 6));
     }
 
     [Fact]
     public void Test3()
     {
-        Assert.Equal(new[] { 0, 1 },
-            TwoSum(new[] { 3, 6 }, 6));
+        Assert.Throws<InvalidOperationException>( () => this.TwoSum(new[] { 3, 6 }, 6));
     }
 
     private int[] TwoSum(int[] nums, int target)
     {
-        throw new NotImplementedException();
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (dictionary.ContainsKey(complement))
+            {
+                return new int[] { dictionary[complement], i };
+            }
+
+            dictionary[nums[i]] = i;
+        }
+
+        throw new InvalidOperationException("No two sum solution");
     }
 }
