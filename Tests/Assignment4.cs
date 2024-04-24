1f19d649ebe6b0abee71c7969f2eb137de273974
EnreiKazuri <vasquezmariano01@gmail.com>
2024-04-23 22:30:34 -0400

    Created Assignment4 and Assignment4Test in accordance to the assignment

---
+++ Tests/Assignment4.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal class Assignment4
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int target = nums[i];
+                if (dictionary.TryGetValue(target, out var seen))
+                {
+                    return true;
+                }
+                dictionary[nums[i]] = i;
+            }
+            return false;
+        }
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            /*According to the original Excercise's constraint,
+            s and t will always be lowercase (https://leetcode.com/problems/valid-anagram/description/).
+            To make sense of this, I won't be evaluating cases, bringing everything to lower case
+             */
+            foreach (char c in s.ToLower())
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+                }
+            }
+
+            foreach (char c in t.ToLower())
+            {
+                if (!charCount.ContainsKey(c) || charCount[c] == 0)
+                {
+                    return false;
+                }
+                charCount[c]--;
+            }
+            return true;
+        }
+    }
+}
---
+++ Tests/Assignment4Test.cs
@@ -0,0 +1,92 @@
+﻿using NuGet.Frameworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class Assignment4Test
+    {
+        Assignment4 proof = new Assignment4();
+        #region Duplicate Tests
+        [Fact]
+        public void No_Duplicates()
+        {
+            int[] nums = { 1, 2, 3, 4, 5, };
+            Assert.False(proof.ContainsDuplicate(nums));
+        }
+
+        [Fact]
+        public void There_are_Duplicates()
+        {
+            int[] nums = { 1, 1, 2, 3, 4, 5, 5 };
+            Assert.True(proof.ContainsDuplicate(nums));
+        }
+        [Fact]
+        public void Lots_No_Duplicates()
+        {
+            int[] nums = { 1, 5, 9, 10, 15, 50, 2, 78, 3, 8, 99 };
+            Assert.False(proof.ContainsDuplicate(nums));
+        }
+        [Fact]
+        public void Lots_With_Duplicates()
+        {
+            int[] nums = { 1, 5, 9, 10, 1, 50, 2, 35, 2, 40, 99 };
+            Assert.True(proof.ContainsDuplicate(nums));
+        }
+        #endregion
+        #region Anagram Tests
+        [Fact]
+        public void Is_Anagram()
+        {
+            string s = "anagrama";
+            string t = "ngramaaa";
+            Assert.True(proof.IsAnagram(s, t));
+        }
+        [Fact]
+        public void Not_Anagram()
+        {
+            string s = "anna";
+            string t = "annn";
+            Assert.False(proof.IsAnagram(s, t));
+        }
+        [Fact]
+        public void Anagram_With_Mayus()
+        {
+            string s = "Anagrama";
+            string t = "ngramaaa";
+            Assert.True(proof.IsAnagram(s, t));
+        }
+        [Fact]
+        public void Not_Anagram_With_Mayus()
+        {
+            string s = "NolocOmICOdIgOCoRrIAAyEr";
+            string t = "sUreBRolOqUeTuDiGaVaDeAh";
+            Assert.False(proof.IsAnagram(s, t));
+        }
+        [Fact]
+        public void Different_Sizes()
+        {
+            string s = "firststring";
+            string t = "firststringg";
+            Assert.False(proof.IsAnagram(s, t));
+        }
+        [Fact]
+        public void Anagran_With_Spaces()
+        {
+            string s = "Ana is Here";
+            string t = "  AnaisHere";
+            Assert.True(proof.IsAnagram(s, t));
+        }
+        [Fact]
+        public void Special_Character()
+        {
+            string s = "N@nΘfúa@gmail.com";
+            string t = "@Θú@Nnfagmail.com";
+            Assert.True(proof.IsAnagram(s, t));
+        }
+        #endregion
+    }
+}
