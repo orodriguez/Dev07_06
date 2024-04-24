4e14408c1ef552077da9d5ebb494163acc77045f
Michael Gonzalez <michael13117764@gmail.com>
2024-04-23 23:05:31 -0400

    ContainsDuplicateTests.cs and IsAnagramTests.cs Added

---
+++ Tests/ContainsDuplicateTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Tests;
+
+public class ContainsDuplicateTests
+{
+        [Fact]
+           public void Contains_Duplicate_True()
+           {
+               
+               Assert.True(ContainsDuplicate(new[] {1,2,3,1}));
+           }
+           
+           [Fact]
+           public void Contains_Duplicate_False()
+           {
+               Assert.False(ContainsDuplicate(new[] { 1, 2, 3, 4 }));
+           }
+           
+           [Fact]
+           public void Contains_Duplicate_MultipleNumbers()
+           {
+               Assert.True(ContainsDuplicate(new[] { 1,1,1,3,3,4,3,2,4,2 }));
+           }
+           
+           [Fact]
+           public void Contains_Duplicate_Empty()
+           {
+               Assert.False(ContainsDuplicate(new int[] {}));
+           }
+
+    public bool ContainsDuplicate(int[] nums)
+    { 
+        var myDictionary = new Dictionary<int, int>(); 
+        foreach (var number in nums) 
+        {
+            if (myDictionary.ContainsValue(number))
+                return true;
+
+            myDictionary[number] = number;
+        }
+        return false;
+    }
+}
---
+++ Tests/IsAnagramTests.cs
@@ -0,0 +1,64 @@
+﻿namespace Tests;
+
+public class IsAnagramTests
+{
+    [Fact]
+    public void IsAnagram_True()
+    {
+        var first = "anagram";
+        var second = "nagaram";
+        Assert.True(IsAnagram(first, second));
+    }
+    
+    [Fact]
+    public void IsAnagram_False()
+    {
+        var first = "car";
+        var second = "rat";
+        Assert.False(IsAnagram(first, second));
+    }
+    
+    [Fact]
+    public void IsAnagram_Caps()
+    {
+        var first = "NacioNaLista ";
+        var second = "altisonancia";
+        Assert.False(IsAnagram(first, second));
+    }
+    
+    [Fact]
+    public void IsAnagram_LengthDiff()
+    {
+        var first = "carro";
+        var second = "rat";
+        Assert.False(IsAnagram(first, second));
+    }
+
+
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length)
+            return false;
+        
+        var myDictionary = new Dictionary<char, int>();
+
+        foreach (var character in s)
+        {
+            if (myDictionary.ContainsKey(character))
+                myDictionary[character]++;
+            else
+                myDictionary[character] = 1;
+        }
+
+        foreach (var character in t)
+        {
+            if (myDictionary.ContainsKey(character))
+                myDictionary[character]--;
+            else
+                return false;
+        }
+
+        
+        return myDictionary.Values.All(v => v == 0);
+
+    }
+}
