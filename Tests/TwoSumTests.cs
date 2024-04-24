933508a6ffc3dde32ea741d3996febe30b3b03ed
acostajoe <121851669+acostajoe@users.noreply.github.com>
2024-04-23 23:51:57 -0400

    Tarea3

--- Tests/TwoSumTests.cs
+++ Tests/TwoSumTests.cs
@@ -4,28 +4,38 @@ namespace Tests;
 public class TwoSumTests
 {
     [Fact]
-    public void Test1()
+    public void Prueba1()
     {
         Assert.Equal(new[] { 0, 1 },
-            TwoSum(new[] { 2, 6, 7, 11, 15 }, 9));
+            Sumando(new[] { 2, 7, 11, 15 }, 9));
     }
 
     [Fact]
-    public void Test2()
+    public void Prueba2()
     {
         Assert.Equal(new[] { 1, 2 },
-            TwoSum(new[] { 3, 2, 4 }, 6));
+            Sumando(new[] { 3, 2, 4 }, 6));
     }
 
     [Fact]
-    public void Test3()
+    public void Prueba3()
     {
         Assert.Equal(new[] { 0, 1 },
-            TwoSum(new[] { 3, 3 }, 6));
+            Sumando(new[] { 3, 3 }, 6));
     }
 
-    private int[] TwoSum(int[] nums, int target)
+    private int[] Sumando(int[] nums, int target)
     {
-        throw new NotImplementedException();
+        var mago = new Dictionary<int, int>();
+        for (var ox = 0; ox < nums.Length; ox++)
+        {
+            var actual = nums[ox];
+            var arreglando = target - actual;
+            if (mago.TryGetValue(arreglando, out var sinValor))
+                return new[] { sinValor, ox };
+            mago[actual] = ox;
+        }
+
+        return Array.Empty<int>();
     }
 }
