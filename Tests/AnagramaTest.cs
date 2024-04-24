16e7bed102ba1c0c87bc36afb991b3967fe9e0ff
Manases Lovera <manaseslovera07@gmail.com>
2024-04-23 22:20:40 -0400

    anagram and contains duplicate aded (test)

---
+++ Tests/AnagramaTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class AnagramaTest
+    {
+        [Fact]
+        public void Is_Anagram()
+        {
+            string s = "ana";
+            string t = "aan";
+
+            Assert.True(IsAnagram(s, t));
+        }
+        [Fact]
+        public void IsNot_Anagram()
+        {
+            string s = "car";
+            string t = "rat";
+
+            Assert.False(IsAnagram(s, t));
+        }
+
+        [Fact]
+        public void IsUpper_Anagram()
+        {
+            string s = "Anagram";
+            string t = "grAnmAA";
+
+            Assert.True(IsAnagram(s, t));
+        }
+
+
+        private bool IsAnagram(string s, string t)
+        {
+
+            char[] sArray = s.ToLower().ToCharArray();
+            char[] tArray = t.ToLower().ToCharArray();
+            
+            Array.Sort(sArray);
+            Array.Sort(tArray);
+
+            return new string(sArray) == new string(tArray);
+        }
+    }
+}
---
+++ Tests/ContainsDuplicateTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class ContainsDuplicateTest
+    {
+        [Fact]
+        public void ContainDuplicate()
+        {
+            Assert.True(ContainsDuplicate([1, 2, 1, 3]));
+        }
+        [Fact] public void ContainDuplicate2()
+        {
+            Assert.True(ContainsDuplicate([1, 1, 2, 6, 8, 4, 4]));
+        }
+        [Fact]
+        public void Doesnt_ContainDuplicate()
+        {
+            Assert.False(ContainsDuplicate([1, 2, 3, 4]));
+        }
+        public bool ContainsDuplicate(int[] nums)
+        {
+            if(nums.GetType() != typeof(int[]))
+            {
+                throw new InvalidDataException();
+            }
+            Dictionary<int, int> hashMap = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hashMap.ContainsKey(nums[i]))
+                {
+                    return true;
+                }
+                hashMap[nums[i]] = i;
+            }
+            return false;
+        }
+    }
+}
