824b3efdeff248f7db2865c1acefd4152547b2e4
“orodriguez” <omarjavier15@gmail.com>
2024-04-24 10:55:10 -0400

    Add Tree

---
+++ Implementations/Tree.cs
@@ -0,0 +1,69 @@
+namespace Implementations;
+
+public class Tree<T>
+{
+    private TreeNode? _root;
+
+    public Tree() =>
+        _root = null;
+
+    public Tree(T rootValue) =>
+        _root = new TreeNode(rootValue);
+
+    public int Count() => _root?.Count() ?? 0;
+
+    public Tree<T> Add(T value) =>
+        Add(new TreeNode(value));
+
+    private Tree<T> Add(TreeNode node)
+    {
+        if (_root == null)
+        {
+            _root = node;
+            return this;
+        }
+
+        _root.Add(node);
+        return this;
+    }
+
+    public Tree<T> Add(T value, Action<TreeNode> action)
+    {
+        var node = new TreeNode(value);
+        Add(node);
+        action(node);
+        return this;
+    }
+
+    public class TreeNode
+    {
+        public T Value { get; set; }
+        private IList<TreeNode> Children { get; set; }
+
+        public TreeNode(T value)
+        {
+            Value = value;
+            Children = new List<TreeNode>();
+        }
+
+        public int Count() =>
+            Children.Sum(node => node.Count()) + 1;
+
+        public TreeNode Add(T value) =>
+            Add(new TreeNode(value));
+
+        public TreeNode Add(TreeNode node)
+        {
+            Children.Add(node);
+            return this;
+        }
+
+        public TreeNode Add(T value, Action<TreeNode> action)
+        {
+            var node = new TreeNode(value);
+            Add(node);
+            action(node);
+            return this;
+        }
+    }
+}
---
+++ Tests/TreeNodeTests.cs
@@ -0,0 +1,63 @@
+using Implementations;
+
+namespace Tests;
+
+public class TreeNodeTests
+{
+    [Fact]
+    public void Count()
+    {
+        var tree = new Tree<string>();
+        
+        Assert.Equal(0, tree.Count());
+    }
+    
+    [Fact]
+    public void Count_SingleNode()
+    {
+        var tree = new Tree<string>("SuperMarket");
+
+        Assert.Equal(1, tree.Count());
+    }
+    
+    [Fact]
+    public void Count_WithLeaf()
+    {
+        var tree = new Tree<string>("SuperMarket");
+
+        tree.Add("Vegetables");
+
+        Assert.Equal(2, tree.Count());
+    }
+    
+    [Fact]
+    public void Count_SubTree()
+    {
+        var tree = new Tree<string>("SuperMarket");
+
+        tree.Add("Vegetables", node => 
+            node.Add("Tomato"));
+
+        Assert.Equal(3, tree.Count());
+    }
+    
+    [Fact]
+    public void Count_ManyNodes()
+    {
+        var tree = new Tree<string>("SuperMarket");
+
+        tree
+            .Add("Vegetables", vegetables =>
+            {
+                vegetables.Add("Tomato");
+                vegetables.Add("Lettuce");
+            })
+            .Add("PersonalCare", personalCare =>
+            {
+                personalCare.Add("Shampoo", shampoo => 
+                    shampoo.Add("HeadAndShoulders"));
+            });
+
+        Assert.Equal(7, tree.Count());
+    }
+}
