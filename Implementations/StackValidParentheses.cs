451fb77b1f07ab9bce8eaf24ac3e2819c9487264
erickdavestech <erickdavestech@gmail.com>
2024-04-25 21:00:54 -0700

    Assignment 5-Daves
    StackValidParentheses

---
+++ Implementations/StackValidParentheses.cs
@@ -0,0 +1,29 @@
+﻿namespace Implementations;
+public class StackValidParentheses
+{
+   public bool StackValidator(string s_validating)
+   {
+      // Puedo usar var por Stack, pero ! Nah xD
+      Stack<char> StackDataStructure = new Stack<char>();
+      foreach (char character in s_validating)
+      {
+         if (character == '(' || character == '[' || character =='{') 
+         {
+            StackDataStructure.Push(character);
+         }
+         else if (character == ')' || character == ']' || character == '}')
+         {
+            if (StackDataStructure.Count == 0)
+               return false;
+            char up = StackDataStructure.Pop();
+            if ((character ==')' && up != '(' ) ||
+            (character ==']' && up != '[' ) ||
+               (character =='}' && up != '{' ))
+            {
+               return false;
+            }
+         }
+      }
+      return StackDataStructure.Count==0;
+   }
+}   
---
+++ Tests/StackValidParenthesesTest.cs
@@ -0,0 +1,24 @@
+﻿using Implementations;
+namespace Tests;
+public class StackValidParenthesesTest
+{
+    [Fact]
+    public void TestValid()
+    {
+        StackValidParentheses Validator = new StackValidParentheses();
+        Assert.True(Validator.StackValidator("()"));
+        Assert.True(Validator.StackValidator("()[]{}"));
+        Assert.True(Validator.StackValidator("{[()]}"));
+        Assert.True(Validator.StackValidator("{[()]}"));
+        Assert.True(Validator.StackValidator(""));
+    }
+    [Fact]
+    public void TestInvalid()
+    {
+        StackValidParentheses Validator = new StackValidParentheses();
+        Assert.False(Validator.StackValidator("("));
+        Assert.False(Validator.StackValidator(")"));
+        Assert.False(Validator.StackValidator("([)]"));
+        Assert.False(Validator.StackValidator("((()"));
+    }
+}
