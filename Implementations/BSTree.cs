64022ba9db5ebb12a44bb516e1136ed981f525d8
“orodriguez” <omarjavier15@gmail.com>
2024-04-30 15:23:43 -0400

    Add Complete BSTree and Graph implementations.

---
+++ Implementations/BSTree.cs
@@ -0,0 +1,50 @@
+using AAD;
+
+namespace Implementations;
+
+public class BSTree
+{
+    private BSTreeNode? _root;
+
+    public BSTree() : this(null)
+    {
+    }
+
+    private BSTree(BSTreeNode? root) =>
+        _root = root;
+
+    public void Add(int value)
+    {
+        if (_root == null)
+        {
+            _root = new BSTreeNode(value);
+            return;
+        }
+        
+        _root.Add(value);
+    }
+
+    public int Count()
+    {
+        if (_root == null)
+            return 0;
+
+        return _root.Count();
+    }
+
+    public bool Contains(int value)
+    {
+        if (_root == null)
+            return false;
+        
+        return _root.Contains(value);
+    }
+
+    public void Delete(int valueToDelete)
+    {
+        if (_root == null)
+            return;
+
+        _root = _root.Delete(valueToDelete);
+    }
+}
--- Implementations/BSTreeNode.cs
+++ Implementations/BSTreeNode.cs
@@ -1,10 +1,12 @@
-namespace Implementations;
+namespace AAD;
 
 public class BSTreeNode
 {
     public int Value { get; set; }
     public BSTreeNode? Left { get; set; }
     public BSTreeNode? Right { get; set; }
+    public int? LeftValue => Left?.Value;
+    public int? RightValue => Right?.Value;
 
     public BSTreeNode(int value)
     {
@@ -13,45 +15,135 @@ public class BSTreeNode
         Right = null;
     }
 
-    public void Add(int value)
+    public void Add(int newValue)
     {
-        if (Value == value)
+        if (Value == newValue)
             return;
 
-        if (Value > value)
+        if (Left != null && Value > newValue)
         {
-            if (Left == null)
-            {
-                Left = new BSTreeNode(value);
-                return;
-            }
-            
-            Left.Add(value);
+            Left.Add(newValue);
             return;
         }
 
-        if (Right == null)
+        if (Value > newValue)
         {
-            Right = new BSTreeNode(value);
+            Left = new BSTreeNode(newValue);
             return;
         }
-        
-        Right.Add(value);
+
+        if (Right != null)
+        {
+            Right.Add(newValue);
+            return;
+        }
+
+        Right = new BSTreeNode(newValue);
     }
 
-    public int LeftValue => Left!.Value;
-    public int RightValue => Right!.Value;
-
-    public bool Contains(int value)
+    public static BSTreeNode From(int[] values)
     {
-        if (Value == value)
+        var root = new BSTreeNode(values.First());
+
+        foreach (var value in values.Skip(1))
+            root.Add(value);
+
+        return root;
+    }
+
+    // O(log n)
+    public bool Contains(int searchedValue)
+    {
+        if (Value == searchedValue)
             return true;
 
-        if (Value > value)
-            return Left != null && Left.Contains(value);
+        if (Value > searchedValue && Left == null)
+            return false;
 
-        return Right != null && Right.Contains(value);
+        if (Value > searchedValue && Left != null)
+            return Left.Contains(searchedValue);
+
+        return Right != null && Right.Contains(searchedValue);
     }
 
-    public int Min() => Left?.Min() ?? Value;
+    // O(n)
+    public void TraverseInOrder(Action<BSTreeNode> action)
+    {
+        Left?.TraverseInOrder(action);
+        action(this);
+        Right?.TraverseInOrder(action);
+    }
+
+    public void TraversePreOrder(Action<BSTreeNode> action)
+    {
+        action(this);
+        Left?.TraversePreOrder(action);
+        Right?.TraversePreOrder(action);
+    }
+
+    public void TraversePostOrder(Action<BSTreeNode> action)
+    {
+        Left?.TraversePostOrder(action);
+        Right?.TraversePostOrder(action);
+        action(this);
+    }
+
+    // Best: O(log n), Worst: O(n)
+    public int Min() => 
+        Left?.Min() ?? Value;
+    
+    // Best: O(log n), Worst: O(n)
+    public int Max() => 
+        Right?.Max() ?? Value;
+
+    public BSTreeNode? Delete(int valueToDelete)
+    {
+        if (Value > valueToDelete && Left != null)
+        {
+            Left = Left.Delete(valueToDelete);
+            return this;
+        }
+
+        if (Value < valueToDelete && Right != null)
+        {
+            Right = Right.Delete(valueToDelete);
+            return this;
+        }
+
+        if (Value != valueToDelete)
+            return this;
+        
+        // Value == value cases
+        
+        if (Left == null && Right == null)
+            return null;
+        
+        if (Left == null)
+            return Right;
+        
+        if (Right == null)
+            return Left;
+
+        var minRight = Right.Min();
+        var newNode = new BSTreeNode(minRight)
+        {
+            Left = Left,
+            Right = Right.Delete(minRight)
+        };
+        return newNode;
+    }
+
+    public int Count()
+    {
+        var count = 0;
+        TraverseInOrder(_ => count++);
+        return count;
+    }
+
+    public int[] ToArray()
+    {
+        var result = new List<int>();
+        TraverseInOrder(node => result.Add(node.Value));
+        return result.ToArray();
+    }
 }
